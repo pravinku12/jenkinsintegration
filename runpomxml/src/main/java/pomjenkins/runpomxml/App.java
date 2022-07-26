ad1003346642cee8e75af5996af1e2636b9b03c0
dell <dell@dell-PC>
2022-07-27 01:35:53 +0530

    push code

---
+++ runpomxml/src/main/java/pomjenkins/runpomxml/App.java
@@ -0,0 +1,13 @@
+package pomjenkins.runpomxml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ runpomxml/src/test/java/pomjenkins/runpomxml/class1.java
@@ -0,0 +1,12 @@
+package pomjenkins.runpomxml;
+
+import org.testng.annotations.Test;
+
+public class class1 {
+
+	@Test
+	public void testcase1() {
+		
+		System.out.println("test case first");
+	}
+}
